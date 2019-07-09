951a2325fda5880a33c9debabe406312c4fdabf7
Stef Spears <spears.sj87@gmail.com>
2019-07-09 04:42:25 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,5 @@
-# Your code here
+def meal_choice(veg1, veg2, protein="meat")
+  irb(main):006:1> puts "What a nutritious meal!"
+irb(main):007:1> puts "A plate of #{protein} with #{veg1} and #{veg2}."
+irb(main):008:1> p "A plate of #{protein} with #{veg1} and #{veg2}."
+irb(main):009:1> end
